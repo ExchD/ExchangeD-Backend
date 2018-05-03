57f9c53da0fb949cb84d44163d02b8ff5e29d915
Yi Wen <wenyi189@gmail.com>
2018-05-03 18:06:11 -0400

    Project Gradle Skeleton

---
+++ src/main/java/com/ExchD/ExchangeD/ExchangeDApplication.java
@@ -0,0 +1,12 @@
+package com.ExchD.ExchangeD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeDApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ExchD/ExchangeD/ExchangeDApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ExchD.ExchangeD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExchangeDApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
